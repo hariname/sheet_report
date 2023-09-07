00737e23e2bc6bc3233aad0b759f642b91e76297
sanjay <sanjaypython22@gmail.com>
2023-09-07 11:46:05 +0530

    'setting'

--- sheet/settings.py
+++ sheet/settings.py
@@ -63,13 +63,25 @@ WSGI_APPLICATION = 'sheet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'sheet_report',
+		'USER': 'sheet_report_USER',
+		'PASSWORD': 'qwerty123',
+		'HOST':'localhost',
+		'PORT':'3306',
+	}
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
