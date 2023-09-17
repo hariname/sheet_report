3103a4356af8412af5e29778343444b918654ce5
sanjay <sanjaypython22@gmail.com>
2023-09-17 12:31:34 +0530

    'siteinfor'

--- Report/admin.py
+++ Report/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
-from .models import SheetReport
+
+from .models import SheetReport, SiteInfo
+
+
 # Register your models here.
 
 class SheetAdmin(admin.ModelAdmin):
     list_display = ('id', 'job_no', 'name')
+
+
+class SiteAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+admin.site.register(SiteInfo, SiteAdmin)
 admin.site.register(SheetReport, SheetAdmin)
